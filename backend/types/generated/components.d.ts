75135c306b4084b0ca7b1084c3351b7f87f2a0a5
khoa.nguyen <khoa.nguyen@logixtek.com>
2024-05-31 16:20:46 +0700

    part 2: done

--- backend/types/generated/components.d.ts
+++ backend/types/generated/components.d.ts
@@ -1,5 +1,37 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface ComponentsLink extends Schema.Component {
+  collectionName: 'components_components_links';
+  info: {
+    displayName: 'Link';
+    icon: '';
+  };
+  attributes: {
+    url: Attribute.String;
+    text: Attribute.String;
+    isExternal: Attribute.Boolean & Attribute.DefaultTo<false>;
+  };
+}
+
+export interface LayoutHeroSection extends Schema.Component {
+  collectionName: 'components_layout_hero_sections';
+  info: {
+    displayName: 'Hero Section';
+    description: '';
+  };
+  attributes: {
+    image: Attribute.Media;
+    heading: Attribute.String;
+    subHeading: Attribute.Text;
+    link: Attribute.Component<'components.link'>;
+  };
+}
+
 declare module '@strapi/types' {
-  export module Shared {}
+  export module Shared {
+    export interface Components {
+      'components.link': ComponentsLink;
+      'layout.hero-section': LayoutHeroSection;
+    }
+  }
 }
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,48 @@
-import { Button } from "@/components/ui/button";
+import { HeroSection } from "@/components/custom/HeroSection";
+import { flattenAttributes } from "@/lib/utils";
+import qs from "qs";
 
-export default function Home() {
+const homePageQuery = qs.stringify({
+  populate: {
+    blocks: {
+      populate: {
+        image: {
+          fields: ["url", "alternativeText"],
+        },
+        link: {
+          populate: true,
+        },
+      },
+    },
+  },
+});
+
+async function getStrapiData(path: string) {
+  const baseUrl = "http://localhost:1337";
+
+  const url = new URL(path, baseUrl);
+  url.search = homePageQuery;
+
+  try {
+    const response = await fetch(url.href, { cache: "no-store" });
+    const data = await response.json();
+    const flattenedData = flattenAttributes(data);
+    return flattenedData;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export default async function Home() {
+  const strapiData = await getStrapiData("/api/home-page");
+
+  const { blocks } = strapiData;
+
+  console.dir(blocks, { depth: null });
+
   return (
-    <main className="container mx-auto py-6">
-      <Button>Our Cool Button</Button>
+    <main>
+      <HeroSection data={blocks[0]} />
     </main>
   );
 }
