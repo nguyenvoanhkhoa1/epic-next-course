0297850e2ccd92e0fceca5b0a4d081715eda9935
khoa.nguyen <khoa.nguyen@logixtek.com>
2024-06-07 18:04:52 +0700

    chap 4: update

--- frontend/src/components/custom/Header.tsx
+++ frontend/src/components/custom/Header.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import { Logo } from "@/components/custom/Logo";
 import { Button } from "@/components/ui/button";
+import { LogoutButton } from "./LogoutButton";
+import { getUserMeLoader } from "@/data/services/get-user-me-loader";
 
 interface HeaderProps {
   data: {
@@ -17,15 +19,37 @@ interface HeaderProps {
   };
 }
 
+interface AuthUserProps {
+  username: string;
+  email: string;
+}
+
+export function LoggedInUser({ userData }: { readonly userData: AuthUserProps }) {
+  return (
+    <div className="flex gap-2">
+      <Link href="/dashboard/account" className="font-semibold hover:text-primary">
+        {userData.username}
+      </Link>
+      <LogoutButton />
+    </div>
+  );
+}
+
 export async function Header({ data }: Readonly<HeaderProps>) {
+  const user = await getUserMeLoader();
+  console.log(user);
   const { logoText, ctaButton } = data;
   return (
     <div className="flex items-center justify-between px-4 py-3 bg-white shadow-md dark:bg-gray-800">
       <Logo text={logoText.text} />
       <div className="flex items-center gap-4">
-        <Link href={ctaButton.url}>
-          <Button>{ctaButton.text}</Button>
-        </Link>
+        {user.ok ? (
+          <LoggedInUser userData={user.data} />
+        ) : (
+          <Link href={ctaButton.url}>
+            <Button>{ctaButton.text}</Button>
+          </Link>
+        )}
       </div>
     </div>
   );
--- frontend/src/components/custom/HeroSection.tsx
+++ frontend/src/components/custom/HeroSection.tsx
@@ -1,38 +1,44 @@
-/* eslint-disable @next/next/no-img-element */
 import Link from "next/link";
-import { StrapiImage } from "../StrapiImage";
+import { getUserMeLoader } from "@/data/services/get-user-me-loader";
+import { StrapiImage } from "@/components/custom/StrapiImage";
 
-interface Image {
+interface ImageProps {
   id: number;
   url: string;
-  alternativeText: string | null;
+  alternativeText: string;
 }
 
-interface Link {
+interface LinkProps {
   id: number;
   url: string;
   text: string;
 }
 
 interface HeroSectionProps {
-  id: number;
-  __component: string;
-  heading: string;
-  subHeading: string;
-  image: Image;
-  link: Link;
+  data: {
+    id: number;
+    __component: string;
+    heading: string;
+    subHeading: string;
+    image: ImageProps;
+    link: LinkProps;
+  };
 }
 
-export function HeroSection({ data }: { readonly data: HeroSectionProps }) {
+export async function HeroSection({ data }: Readonly<HeroSectionProps>) {
+  const user = await getUserMeLoader();
   const { heading, subHeading, image, link } = data;
-  const imageURL = "http://localhost:1337" + image.url;
+
+  const userLoggedIn = user.ok;
+  const linkUrl = userLoggedIn ? "/dashboard" : link.url;
+
   return (
     <header className="relative h-[600px] overflow-hidden">
       <StrapiImage
         alt="Background"
-        className="absolute inset-0 object-cover w-full h-full aspect/16:9"
-        src={imageURL}
+        className="absolute inset-0 object-cover w-full h-full"
         height={1080}
+        src={image.url}
         width={1920}
       />
       <div className="relative z-10 flex flex-col items-center justify-center h-full text-center text-white bg-black bg-opacity-20">
@@ -40,9 +46,9 @@ export function HeroSection({ data }: { readonly data: HeroSectionProps }) {
         <p className="mt-4 text-lg md:text-xl lg:text-2xl">{subHeading}</p>
         <Link
           className="mt-8 inline-flex items-center justify-center px-6 py-3 text-base font-medium text-black bg-white rounded-md shadow hover:bg-gray-100"
-          href={link.url}
+          href={linkUrl}
         >
-          {link.text}
+          {userLoggedIn ? "Dashboard" : link.text}
         </Link>
       </div>
     </header>
